5ff2bb33f24a6d3f03097575d22a3c9c20a7d865
CHW_1207 <dilkusha27@gmail.com>
2024-12-09 04:30:39 +0900

    refactor: App.js 환경설정 통한 React-Query 전역 적용

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import MyPage from './pages/MyPage';
 import RollingPaperPage from './pages/RollingPaperPage';
 import RedirectHandler from './pages/RedirectHandler';
 
+// react-query 적용
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 // Dev 전용(개발용)
 import MyPageDev from './pages/MyPageDev';
 import RollingPaperPageDev from './pages/RollingPaperPageDev';
@@ -15,31 +19,36 @@ import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './components/MuiColor';
 // import store from './store.js'
 
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
+
 function App() {
   return (
   <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
     <div className="app-container">
-      {/* <Provider store={store}> */}
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            {process.env.NODE_ENV === 'development' && (
-                <Route path="/mypage-dev" element={<MyPageDev/>} />
-            )}
             <Route path="/mypage" element={<MyPage />} />
             <Route path="/oauth/callback" element={<RedirectHandler />} />ㅅ
             <Route path="/:url" element={<StudentSigninPage />} />
+            <Route path="/roll/:url/join/" element={<RollingPaperPage />} />
+
+            {process.env.NODE_ENV === 'development' && (
+                <Route path="/mypage-dev" element={<MyPageDev/>} />
+            )}
             {process.env.NODE_ENV === 'development' && (
                 <Route path="/studentsigin-dev" element={<StudentSigninPageDev />} />
             )}
             {process.env.NODE_ENV === 'development' && (
                 <Route path="/rolling-paper-dev" element={<RollingPaperPageDev />} />
             )}
-            <Route path="/roll/:url/join/" element={<RollingPaperPage />} />
           </Routes>
         </BrowserRouter>
-      {/* </Provider> */}
-    </div>
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </ThemeProvider>
   );
 }
