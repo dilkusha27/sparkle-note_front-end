a83cde6e38123bfcb0aff2d147f14b1e38bee7d8
dilkusha27 <dilkusha27@gmail.com>
2026-02-10 17:30:09 +0900

    feat: MUI 테마/리셋 및 라우터 프로바이더 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 import './index.css'
 import App from './App.jsx'
 
+const theme = createTheme()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
